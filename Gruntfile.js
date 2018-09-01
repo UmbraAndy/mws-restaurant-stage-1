5534d0680b9ff2e377efd7fcaddbc678683aaa5f
Adeleke Babatunde <adeleke.a.babatunde@gmail.com>
2018-09-01 20:02:40 +0100

    Added fixed width for images for grunt responsive image task

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,25 +6,32 @@ module.exports = function(grunt) {
           options: {
             sizes: [{
               name: '1x',
-              width: '20%',
+              width: '400',
               quality: 20
             },{
               name: '2x',
-              width: '50%',
+              width: '100%',
               quality: 40
             }]
           },
           files: [{
             expand: true,
             src: ['*.{gif,jpg,png}'],
-            cwd: 'img/',
-            dest: 'images/'
+            cwd: 'img_src/',
+            dest: 'img/'
           }]
         }
       },
+    /* delte files in the img dir */
+    clean: {
+        dev: {
+          src: ['images'],
+        },
+      },      
     });
   
     grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.registerTask('default', ['responsive_images']);
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('default', ['clean','responsive_images']);
   
   };
