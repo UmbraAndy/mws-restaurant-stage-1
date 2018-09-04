a9df85886d3ff70a6700cfdcef836cb1be184c0b
Adeleke Babatunde <adeleke.a.babatunde@gmail.com>
2018-09-04 22:48:15 +0100

    Implemented offline experience

--- sw.js
+++ sw.js
@@ -1,5 +1,31 @@
+const localCacheName = 'restaurant-v1';
+
 self.addEventListener('install', installEvent =>{
     console.log('Starting install');
+    const urlsRequestsToCache = [
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/data/restaurants.json',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
+    ]
+
+    //create cache 
+    installEvent.waitUntil(
+        caches.open(localCacheName)
+        .then(cache =>{
+            console.log('Cache created');
+            return cache.addAll(urlsRequestsToCache);
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    );
 
 });
 
@@ -10,5 +36,12 @@ self.addEventListener('activate',activateEvent =>{
 
 
 self.addEventListener('fetch',fetchEvent=>{
-    console.log(fetchEvent);
+    const request = fetchEvent.request;
+    fetchEvent.respondWith(
+        caches.match(request)
+        .then(response =>{
+            if(response) return response;
+            return fetch(request);
+        })
+    )
 })
