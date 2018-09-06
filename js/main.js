4e69f81faa10ef40593b444a495a982505ac1c36
Adeleke Babatunde <adeleke.a.babatunde@gmail.com>
2018-09-06 06:38:27 +0100

    Added proper aria lable for view details button

--- js/main.js
+++ js/main.js
@@ -186,7 +186,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt =  restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -200,6 +200,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('arial-label', 'View detail for '+restaurant.name);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
